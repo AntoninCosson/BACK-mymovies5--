d1c5f9d1dad556cf8b8f7a9e65ea074b9bf79b37
Antonin <cosson.antonin@gmail.com>
2025-07-09 14:45:50 +0200

    back maj

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 var express = require("express");
 const fetch = require("node-fetch");
 const app = express();
-const PORT = 3001;
+// const PORT = 3001;
 const cors = require("cors");
 
 const corsOptions = {
