de9cd60a38fae3bf4e9f3b27d4619cc54c3736dc
Antonin <cosson.antonin@gmail.com>
2025-07-09 15:07:47 +0200

    back maj

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ require("dotenv").config();
 var express = require("express");
 const fetch = require("node-fetch");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: "https://front-mymovies5.vercel.app",
+  })
+);
 
 app.use("/movies, moviesRouter");
 app.use("/", moviesRoutes);
+
 module.exports = router;
