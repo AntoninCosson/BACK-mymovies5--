d3e58fe38ba1009abf1c691192c3e26689e1bc5a
Antonin <cosson.antonin@gmail.com>
2025-07-09 14:26:43 +0200

    cors added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 const PORT = 3001;
+const cors = require("cors");
+
+app.use(cors());
 
 var router = express.Router();
 
